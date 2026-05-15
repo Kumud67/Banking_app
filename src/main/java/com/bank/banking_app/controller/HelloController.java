6c67364eb8e084457f81b72cb88ecca5521d0662
Kumud67 <kumud.t66@gmail.com>
2026-05-16 01:10:31 +0530

    controller

---
+++ src/main/java/com/bank/banking_app/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.bank.banking_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Banking App Running";
+    }
+}
